07222e93091f5dce90974b305dffb86eef29cf56
Abdul Basit <basit.be18@iba-suk.edu.pk>
2021-03-15 10:04:15 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+let fullname = "Abdul Basit"
+let city = "Sukkur"
+let state = "Sindh"
+
+let address = fullname + '\n House no. abc street' + city + '\n' + state 
+
+let address2 = `${ 1 == 1 ? "Hurrah" : OOps } `
+
+console.log(address2)
+
+console.log(address)
