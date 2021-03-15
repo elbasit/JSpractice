c0f212e96256f7111dd56446c88add6d694bebe3
Abdul Basit <63086434+elbasit@users.noreply.github.com>
2021-03-15 15:29:55 +0500

    Add files via upload

---
+++ ObajectArray.js
@@ -0,0 +1,12 @@
+var person = {
+  'man': {
+    firstName: "John",
+    lastName: "Doe",
+    age: 50,
+    arr: [1, 2, 3],
+    eyeColor: "blue"
+  },
+  'girl': "Happy"
+}
+
+console.log(person.girl + " " + person.man.lastName + ' ' + person.man.arr[0])
